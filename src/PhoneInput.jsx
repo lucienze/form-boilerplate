9f761a8b68cd7959947d000b50ccb68d94a8f5ca
lucienze <nastyk4t@gmail.com>
2018-02-10 17:35:52 +0300

    новый Phone Input

--- src/PhoneInput.jsx
+++ src/PhoneInput.jsx
@@ -1,31 +1,29 @@
 import React from 'react';
 import MaskedInput from 'react-text-mask';
 
-class PhoneInput extends React.Component {
-    render() {
-        return (
-            <MaskedInput
-                mask={[
-                    '8',
-                    ' ',
-                    '(',
-                    /[1-9]/,
-                    /\d/,
-                    /\d/,
-                    ')',
-                    ' ',
-                    /\d/,
-                    /\d/,
-                    /\d/,
-                    '-',
-                    /\d/,
-                    /\d/,
-                    /\d/,
-                    /\d/
-                ]}
-            />
-        );
-    }
+const PhoneInput = (props) => {
+    return (
+        <MaskedInput
+            mask={[
+                '+7',
+                ' ',
+                '(',
+                /[1-9]/,
+                /\d/,
+                /\d/,
+                ')',
+                ' ',
+                /\d/,
+                /\d/,
+                /\d/,
+                '-',
+                /\d/,
+                /\d/,
+                /\d/,
+                /\d/
+            ]}
+        />
+    );
 }
 
 export default PhoneInput;
